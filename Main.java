24e1e936b5cadc69e99c9745757a7fed575926bc
Elvin <elvingarayev90@gmail.com>
2023-12-27 22:34:00 +0400

    tests

--- Main.java
+++ Main.java
@@ -1,13 +1,17 @@
 import Library.GUI.*;
 import java.util.List;
 import Library.MovieData.*;
+import Library.UserData.User;
+import Library.UserData.UserDataManagement;
 
 public class Main {
     public static void main(String[] args) {
-        LoginGUI login = new LoginGUI();
+        //LoginGUI login = new LoginGUI();
         new MovieDatabase();
         // System.out.println(MovieDatabase.getMovieDetails("Fight Club",1999));
         List<Movie> a = MovieDatabase.getMovies();
+        UserDataManagement.addUser(new User("salam","salam"));
+
     }
 
 }
